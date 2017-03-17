519f087568cb8e40b000a6a18b9244d37527573d
Tomas Leko <tomas.leko@gmail.com>
2017-03-17 17:50:43 +0100

    Fix size grid.

--- src/App.elm
+++ src/App.elm
@@ -1,18 +1,31 @@
 module App exposing (..)
 
 import Html exposing (Html, text, div, img)
-import Html.Attributes exposing (src)
+import Html.Attributes as H
+import Svg as S
+import Svg.Attributes as S
+
+
+type alias Position =
+    { x : Int
+    , y : Int
+    }
+
+
+type alias Size =
+    { width : Int
+    , height : Int
+    }
 
 
 type alias Model =
-    { message : String
-    , logo : String
+    { size : Size
     }
 
 
 init : String -> ( Model, Cmd Msg )
 init path =
-    ( { message = "Your Elm App is working!", logo = path }, Cmd.none )
+    ( { size = Size 10 10 }, Cmd.none )
 
 
 type Msg
@@ -24,12 +37,34 @@ update msg model =
     ( model, Cmd.none )
 
 
+intsToString : List Int -> String
+intsToString ints =
+    ints
+        |> List.map toString
+        |> List.intersperse " "
+        |> List.foldr (++) ""
+
+
 view : Model -> Html Msg
-view model =
-    div []
-        [ img [ src model.logo ] []
-        , div [] [ text model.message ]
-        ]
+view { size } =
+    let
+        viewBoxVal =
+            intsToString [ 0, 0, size.width, size.height ]
+    in
+        div [ H.style [ ( "padding", "20px" ) ] ]
+            [ S.svg [ S.width "400", S.height "400", S.viewBox viewBoxVal ]
+                (List.range 1 10 |> List.map viewRow)
+            ]
+
+
+viewRow : Int -> S.Svg msg
+viewRow rowIdx =
+    S.g [] (List.range 1 10 |> List.map (viewCell rowIdx))
+
+
+viewCell : Int -> Int -> S.Svg msg
+viewCell rowIdx colIdx =
+    S.rect [ S.x (toString colIdx), S.y (toString rowIdx), S.width "1", S.height "1", S.fill "gray", S.strokeWidth "0.1", S.stroke "white" ] []
 
 
 subscriptions : Model -> Sub Msg
